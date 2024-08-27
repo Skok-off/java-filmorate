5fed0e52ca8ec9fca24b7dfd337cb3af2580d3c3
Skok-off <skokff@yandex.ru>
2024-08-28 01:18:41 +0500

    hotfix: storages - friend, mpa
    -замена JdbcTemplate на NamedParameterJdbcTemplate

--- src/main/java/ru/yandex/practicum/filmorate/storage/mpa/MpaDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mpa/MpaDbStorage.java
@@ -3,12 +3,14 @@ package ru.yandex.practicum.filmorate.storage.mpa;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.mapper.MpaMapper;
 import ru.yandex.practicum.filmorate.model.Mpa;
+
 import java.util.Collection;
+import java.util.Map;
 import java.util.Objects;
 
 @Slf4j
@@ -16,23 +18,23 @@ import java.util.Objects;
 @Repository
 public class MpaDbStorage {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final MpaMapper mapper;
 
     public Mpa findMpa(Long id) {
         if (Objects.isNull(id)) {
             throw new ValidationException("Некорректный идентификатор рейтинга.");
         }
-        String sql = "SELECT * FROM ratings WHERE id = ?";
+        String sql = "SELECT * FROM ratings WHERE id = :id";
         try {
-            return jdbcTemplate.queryForObject(sql, mapper, id);
+            return namedParameterJdbcTemplate.queryForObject(sql, Map.of("id", id), mapper);
         } catch (EmptyResultDataAccessException e) {
             return null;
         }
     }
 
     public Collection<Mpa> findAll() {
-        return jdbcTemplate.query("SELECT * FROM ratings ORDER BY id", mapper);
+        return namedParameterJdbcTemplate.query("SELECT * FROM ratings ORDER BY id", mapper);
     }
 
 }
